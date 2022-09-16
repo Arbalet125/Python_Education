0e8c5053b8446230a0fc13b8aec30f0526c90c88
Arbalet <MametovV@gmail.com>
2022-09-16 21:03:05 +0300

    complete 4 task

--- Seminar4/DZ_seminar4.py
+++ Seminar4/DZ_seminar4.py
@@ -33,3 +33,48 @@
 # new_lst = []
 # [new_lst.append(i) for i in lst if i not in new_lst]
 # print(f"Список из неповторяющихся элементов списка \n", lst, "\nРавен следующему списку: \n", new_lst)
+
+###################################################################################
+
+# Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k. 
+
+# import random
+
+# def write_file(st):
+#     with open('file_dz_seminar4.txt', 'w') as data:
+#         data.write(st)
+
+
+# def rnd():
+#     return random.randint(0,101)
+
+
+# def create_spisok_koeficent(k):
+#     lst = [rnd() for i in range(k+1)]
+#     return lst
+    
+
+# def create_str(sp):
+#     lst= sp[::-1]
+#     wr = ''
+#     if len(lst) < 1:
+#         wr = 'x = 0'
+#     else:
+#         for i in range(len(lst)):
+#             if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+#                 wr += f'{lst[i]}x^{len(lst)-i-1}'
+#                 if lst[i+1] != 0:
+#                     wr += ' + '
+#             elif i == len(lst) - 2 and lst[i] != 0:
+#                 wr += f'{lst[i]}x'
+#                 if lst[i+1] != 0:
+#                     wr += ' + '
+#             elif i == len(lst) - 1 and lst[i] != 0:
+#                 wr += f'{lst[i]} = 0'
+#             elif i == len(lst) - 1 and lst[i] == 0:
+#                 wr += ' = 0'
+#     return wr
+
+# k = int(input("Введите натуральную степень k = "))
+# spisok_koeficent = create_spisok_koeficent(k)
+# write_file(create_str(spisok_koeficent))
